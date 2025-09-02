b64b0ad23cd8ab648b6bc5c51d90af9e2d04ddd2
rafeeqsopan <134214918+rafeeqsopan@users.noreply.github.com>
2025-09-02 23:14:46 +0700

    Add files via upload

---
+++ BAB 2/Balok.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Balok {
+    int p, l, t;
+
+    int volume(int p, int l, int t) {
+        return (p * l * t);
+    }
+
+    public static void main(String[] args) {
+        Scanner masuk = new Scanner(System.in);
+        
+        System.out.print("Panjang = ");
+        int a = masuk.nextInt();
+        
+        System.out.print("Lebar = ");
+        int b = masuk.nextInt();
+        
+        System.out.print("Tinggi = ");
+        int c = masuk.nextInt();
+        
+        Balok coba = new Balok();
+        
+        System.out.print("\nVolume balok = " + coba.volume(a, b, c));
+        
+        masuk.close();
+    }
+}
