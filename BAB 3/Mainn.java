98183e1b9df93cfa5470a7d3abe0e23a868e429b
rafeeqsopan <134214918+rafeeqsopan@users.noreply.github.com>
2025-09-04 21:22:05 +0700

    Update and rename Array3Dimensi.java to Mainn.java

--- BAB 3/Mainn.java
+++ BAB 3/Mainn.java
@@ -1,8 +1,9 @@
 // Percobaan ke-5 : Menghitung panjang baris dan kolom pada array dua dimensi
-public class Array3Dimensi { 
+public class Mainn { 
     public static void main(String args[]) { 
         String[][] data = new String[4][5]; 
         System.out.println("Dimension 1: " + data.length); 
         System.out.println("Dimension 2: " + data[0].length); 
     } 
 }
+
