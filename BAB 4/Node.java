60468d3267393980ff616e7412646e9d983186df
rafeeqsopan <134214918+rafeeqsopan@users.noreply.github.com>
2025-09-09 12:21:24 +0700

    Add files via upload

---
+++ BAB 4/Node.java
@@ -0,0 +1,11 @@
+public class Node{
+    Object data;
+    Node next;
+
+    public static void main(String[] args) {
+        Node head=new Node();
+        head.data= "A";
+        System.out.println("data: "+head.data);
+        System.out.println("pointer: "+head.next);
+    }
+}
